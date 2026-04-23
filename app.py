369dd761cb8f88167763423b107712612ce376e9
arjkrao <arjun.krishna.rao@gmail.com>
2026-04-23 17:53:16 -0400

    Added functionality to get Name and email from Google Auth

--- app.py
+++ app.py
@@ -3,49 +3,77 @@ import pathlib
 from flask import Flask, render_template, request, session, abort, redirect
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
+from sqlalchemy import create_engine, text, URL
 from google_auth_oauthlib.flow import Flow
 from google.oauth2 import id_token
 from google.auth.transport import requests
+from functools import wraps
 
 
 app = Flask(__name__)
 
 load_dotenv()
-db_user = os.environ['USERNAME']
-db_password = os.environ['PASSWORD']
-db_name = os.environ['DBNAME']
-db_hostname = os.environ['HOSTNAME']
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://{db_user}:{db_password}@{db_hostname}/{db_name}".format(db_user=db_user, db_password=db_password, db_hostname=db_hostname, db_name=db_name)
+default_db_url_obj = URL.create(
+    drivername="mysql+pymysql",
+    username=os.environ['DEFAULT_USERNAME'],
+    password=os.environ['DEFAULT_PASSWORD'],
+    host=os.environ['HOSTNAME'],
+    port=os.environ['PORTNUM'],
+    database=os.environ['DBNAME'],
+)
+
+admin_db_url_obj = URL.create(
+    drivername="mysql+pymysql",
+    username=os.environ['ADMIN_USERNAME'],
+    password=os.environ['ADMIN_PASSWORD'],
+    host=os.environ['HOSTNAME'],
+    port=os.environ['PORTNUM'],
+    database=os.environ['DBNAME'],
+)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = default_db_url_obj.render_as_string(hide_password=False)
+app.config['SQLALCHEMY_BINDS'] = {
+    "admin": admin_db_url_obj.render_as_string(hide_password=False)
+}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = "heheheha"
+
+app.secret_key = os.environ['APP_SECRET_KEY']
 
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
-flow = Flow.from_client_secrets_file(client_secrets_file=client_secrets_file,
-                                     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-                                     redirect_uri="http://127.0.0.1:5000/callback")
 
 db = SQLAlchemy(app)
 
 
 def login_is_required(function):
+    @wraps(function)
     def wrapper(*args, **kwargs):
         if "google_id" not in session:
             return abort(401) # Authorization required
-        else:
-            return function()
+        return function(*args, **kwargs)
     return wrapper
+
+def create_flow():
+    return Flow.from_client_secrets_file(
+        client_secrets_file=client_secrets_file,
+        scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+        redirect_uri="http://127.0.0.1:5000/callback"
+    )
 
 @app.route("/login")
 def login():
+    flow = create_flow()
     authorization_url, state = flow.authorization_url()
     session["state"] = state
-    session["google_id"] = "test"
+    session['code_verifier'] = flow.code_verifier
+
+
     return redirect(authorization_url)
 
 @app.route("/callback")
 def callback():
+    flow = create_flow()
+    flow.code_verifier = session.get('code_verifier')
     flow.fetch_token(authorization_response=request.url)
 
     if not session.get('state') == request.args.get('state'):
@@ -61,6 +89,7 @@ def callback():
 
     session['google_id'] = id_info['sub']
     session['name'] = id_info.get('name')
+    session['email'] = id_info.get('email')
 
     return redirect("/protected_area")
 
@@ -76,4 +105,7 @@ def index():
 @app.route("/protected_area")
 @login_is_required
 def protected_area():
-    return "<p> Protected! Welcome {name} <a href='/logout'><button>Logout</button></a></p>".format(name=session.get("name"))
+    return "<p> Protected! Welcome {name}. Your email is {email} <a href='/logout'><button>Logout</button></a></p>".format(
+        name=session.get("name"),
+        email=session.get("email")
+        )
